d7f77a1e44859215a3ac8de8197c8696541ee245
Jonathan <jonmartin721@gmail.com>
2018-01-27 16:59:44 -0600

    Initial commit.

---
+++ src/RPC.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class RPC {
+
+    public static void main(String[] args) {
+
+        System.out.println("Rock Paper Scissors");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
