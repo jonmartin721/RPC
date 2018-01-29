1da25e577fa2951586607e32114daa1d7a6a8744
Jonathan <jonmartin721@gmail.com>
2018-01-28 23:28:13 -0600

    Reformatted and cleaned out output.

--- src/RPC.java
+++ src/RPC.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class RPC {
 
@@ -16,8 +16,7 @@ public class RPC {
         String userChoice = keyboard.next(); //takes user choice
 
         //tests the user's input and displays an invalid message if it doesn't pass, until they enter something valid
-        while (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors"))
-        {
+        while (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors")) {
             System.out.println("Invalid option:");
             System.out.println("\nChoose an option by typing the word:\nrock\npaper\nscissors\n");
             userChoice = keyboard.next(); //takes user choice
@@ -25,15 +24,15 @@ public class RPC {
 
         String computerChoice = aiChoice(); //sets ai choice
 
+        //prints choices and results
         System.out.println("User chose: " + userChoice);
         System.out.println("Computer chose: " + computerChoice);
         String winner = chooseWinner(userChoice, computerChoice);
         System.out.println(winner);
-
     }
 
     //method that chooses a choice for the AI
-    private static String aiChoice(){
+    private static String aiChoice() {
 
         String choice;
         Random rand = new Random();
@@ -57,48 +56,22 @@ public class RPC {
 
         if (userChoice.equals(computerChoice)) {
             return "It's a tie!";
-        }
-
-        else if (userChoice.equals("paper") && computerChoice.equals("rock")) {
+        } else if (userChoice.equals("paper") && computerChoice.equals("rock")) {
             return "You win!";
-        }
-
-        else if (userChoice.equals("paper") && computerChoice.equals("scissors")) {
+        } else if (userChoice.equals("paper") && computerChoice.equals("scissors")) {
             return "Computer wins!";
-        }
-
-        else if (userChoice.equals("rock") && computerChoice.equals("paper")) {
+        } else if (userChoice.equals("rock") && computerChoice.equals("paper")) {
             return "Computer wins!";
-        }
-
-        else if (userChoice.equals("rock") && computerChoice.equals("scissors")) {
+        } else if (userChoice.equals("rock") && computerChoice.equals("scissors")) {
             return "You win!";
-        }
-
-        else if (userChoice.equals("scissors") && computerChoice.equals("paper")) {
+        } else if (userChoice.equals("scissors") && computerChoice.equals("paper")) {
             return "You win!";
-        }
-
-        else if (userChoice.equals("scissors") && computerChoice.equals("rock")) {
+        } else if (userChoice.equals("scissors") && computerChoice.equals("rock")) {
             return "Computer wins!";
         }
 
         return "Didn't select a winner ):";
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
